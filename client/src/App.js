bbe37e44265aab76e8ff85151d7bcb57d950218f
Jeramiah Coffey <jerrygcoffey@gmail.com>
2022-05-04 16:41:51 -0500

    MUI setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import './App.css'
+import { Typography, Container } from '@mui/material'
 
 function App() {
-    return <div>Hello world!</div>
+    return (
+        <Container>
+            <Typography variant='h1'>Hello, from the frontend!</Typography>
+        </Container>
+    )
 }
 
 export default App
